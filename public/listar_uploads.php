6cebfef73800b1b891d896498a8f95fe1d4306c4
leostevanato <47180899+leostevanato@users.noreply.github.com>
2023-08-10 23:08:07 -0300

    Filtrar e inverter ordem da lista de arquivos

--- public/listar_uploads.php
+++ public/listar_uploads.php
@@ -1,9 +1,15 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: *');
+
 $dir = 'uploads';
 
-$arquivos = array_diff(scandir($dir), array('..', '.'));
+$arquivos = array_diff(scandir($dir), array('..', '.', '.htaccess'));
+$arquivos = array_values($arquivos);
 
-echo "<pre>";
-// print_r($arquivos);
+rsort($arquivos);
+
 echo json_encode($arquivos);
-echo "</pre>";
+
+exit();
